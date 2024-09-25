1c26c52d359eeca705a7dffca6c69925b5d5f924
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2024-09-25 09:38:30 -0300

    Add Control & Services

--- controladores/control_livro.js
+++ controladores/control_livro.js
@@ -1,14 +1,14 @@
-
+const { getTodosLivros } = require('../servicos/parse_livros');
 
 function getLivros(req, res) {
   try {
-    res.send("Livros: Hello World!")
+    const livros = getTodosLivros()
+    res.send(livros)
   } catch (error) {
     res.status(500)
     res.send(error.message)
   }
 }
-
 
 module.exports = { 
   getLivros
