286af720dba9441930a75c79198e4766335d58f8
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2024-09-25 09:10:39 -0300

    Add Controller

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,12 +1,11 @@
-const { Router } = require("express")
+const { Router } = require("express");
+const { getLivros } = require("../controladores/control_livro");
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send("Hello World!")
-})
+router.get('/', getLivros)
 
-
+ 
 router.post('/', (req, res) => {
   res.send("Você fez uma requisição POST")
 })
